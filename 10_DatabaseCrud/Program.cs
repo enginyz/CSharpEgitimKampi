9e9a1467f069cf018e87f6d5c6af304e1efb6a40
enginyz <enginyildiz33@gmail.com>
2025-09-23 15:59:10 +0300

    10_DatabaseCrud oluşturuldu

---
+++ 10_DatabaseCrud/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CRUD --> Crate-Read-Update-Delete
+
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Eklemek istediğiniz Kategori Adı: ");
+            string categoryName = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;initial catalog=EgitimKampiDb;integrated security=true");
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)" , connection);
+            command.Parameters.AddWithValue("@p1", categoryName);
+
+            Console.Read();
+        }
+    }
+}
